62c74fdde707933651f2a035478958adbf427bd5
Dennis Heckman <denheck@gmail.com>
2017-02-20 10:15:33 -0600

    refactor conveyer to parse markdown into html

--- src/conveyer.ml
+++ src/conveyer.ml
@@ -1,7 +1,14 @@
 open Core.Std
+open Cow
+
+let parse_markdown markdown = 
+  markdown
+  |> Omd.of_string
+  |> Omd.to_html
 
 let generate directory = 
   let source = In_channel.read_all (directory ^ "/index.md") in
-  Out_channel.write_all ~data:source "tmp/destination/index.html"
+    let parsed_source = parse_markdown source in
+    Out_channel.write_all ~data:parsed_source "tmp/destination/index.html"
 
 let () = generate "tmp/source"
