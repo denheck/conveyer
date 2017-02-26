88c0e5ed5a7bf926bba22bbad17cc78cba4c2461
Dennis Heckman <denheck@gmail.com>
2017-02-26 17:55:44 -0600

    change .md extensions to .html

--- src/conveyer.ml
+++ src/conveyer.ml
@@ -3,23 +3,27 @@ open Cow
 
 let (/^) a b = Filename.concat a b
 
-let parse_markdown markdown = 
+let parse_markdown markdown =
   markdown
   |> Omd.of_string
   |> Omd.to_html
 
-let parse_file filename filepath contents = 
+let parse_file filename filepath contents =
   let () = Unix.mkdir_p filepath in
-    Out_channel.write_all (filepath /^ filename) (parse_markdown contents)
+  let output_file = (Filename.chop_extension (filepath /^ filename)) ^ ".html" in
+      Out_channel.write_all output_file (parse_markdown contents)
 
-let command = 
-  Command.basic 
+let generate_site source destination =
+  Directory_tree.walk_files parse_file (Directory_tree.make_file_tree source) destination
+
+let command =
+  Command.basic
     ~summary:"Conveyer static site generator"
     Command.Spec.(
       empty
       +> flag "-s" (required file) ~doc:"Source path"
       +> flag "-d" (required file) ~doc:"Destination path"
     )
-    (fun source destination () -> Directory_tree.walk_files parse_file (Directory_tree.make_file_tree source) destination)
+    (fun source destination () -> ignore (generate_site source destination))
 
 let () = Command.run command
