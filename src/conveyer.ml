aae5cb50b9f665ec835b6586ce489d26076b51cb
Dennis Heckman <denheck@gmail.com>
2017-02-26 08:38:11 -0600

    generate static site from directory of markdown files

--- src/conveyer.ml
+++ src/conveyer.ml
@@ -6,18 +6,31 @@ let parse_markdown markdown =
   |> Omd.of_string
   |> Omd.to_html
 
-let parse_file file =
-  In_channel.read_all file
-  |> parse_markdown
-
 let (/^) a b = Filename.concat a b
 
-let list_files directory =
-  Sys.readdir directory
-  |> Array.to_list
+exception UnkownFileType of string
 
-let generate directory = 
-  list_files directory
-  |> List.map ~f:(fun file -> Out_channel.write_all ("tmp/destination" /^ file) (parse_file (directory /^ file)))
+type file_tree = 
+  | File of string * string
+  | Directory of string * file_tree list
 
-let () = ignore(generate "tmp/source")
+let rec make_file_tree path =
+  let name = Filename.basename path in
+    match Sys.is_directory path with
+    | `Yes -> Directory (name, (List.map ~f:(fun name -> make_file_tree (path /^ name)) (Array.to_list (Sys.readdir path))))
+    | `No -> File (name, In_channel.read_all path)
+    | _   -> raise (UnkownFileType ("Unknown or missing file " ^ path))
+    
+let rec walk_files fn directory_tree path =
+  match directory_tree with
+  | File (name, contents)        -> ignore (fn name path contents)
+  | Directory (name, file_trees) -> ignore (List.map ~f:(fun directory_tree -> walk_files fn directory_tree (path /^ name)) file_trees)
+
+let destination = "tmp/destination"
+let source = "tmp/source"
+
+let parse_file filename filepath contents = 
+  let () = Unix.mkdir_p filepath in
+    Out_channel.write_all (filepath /^ filename) (parse_markdown contents)
+
+let () = ignore (walk_files parse_file (make_file_tree source) destination)
